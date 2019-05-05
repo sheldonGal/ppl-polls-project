7235db573fe9a3b2c4e78fa0ca5b7a7cb9098785
Gal Sheldon <sheldon.gal.nb@gmail.com>
2019-05-05 05:57:13 +0300

    project runs with new 'dev.py' settings file

--- ppl/manage.py
+++ ppl/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ppl.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ppl.settings.dev') # Change made so the dev settings will run
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
